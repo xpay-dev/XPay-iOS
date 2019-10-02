34fd78241386eca3f2e8ac9fddeda45bfa269984
Maryow <mario@xpay.world>
2019-10-02 21:36:35 +0800

    initial commit

---
+++ Xpay World/Scene/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  Xpay World
+//
+//  Created by Mario Juni on 02/10/2019.
+//  Copyright © 2019 Mario Juni. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/).foregroundColor(.black).bold()
+            .font(.largeTitle)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
